e9a3943a309f3694b13ccedb1360cf0d6b25f9ab
Pedro Queiroga <pedrosqueiroga@gmail.com>
2023-06-06 19:26:34 -0300

    first try

---
+++ docs/common.d274a04fd6faf7fc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkqr_code_reader=self.webpackChunkqr_code_reader||[]).push([[592],{729:(n,t,r)=>{r.d(t,{N:()=>o});var c=r(902);let o=(()=>{class e{constructor(){this.text=""}}return e.\u0275fac=function(a){return new(a||e)},e.\u0275prov=c.Yz7({token:e,factory:e.\u0275fac,providedIn:"root"}),e})()}}]);
