6b5c5a56f0004ee546539f9dda9ae4438816c37f
Pedro Queiroga <pedrosqueiroga@gmail.com>
2023-06-06 22:18:48 -0300

    first try

--- src/app/reader/reader.component.ts
+++ src/app/reader/reader.component.ts
@@ -1,22 +1,23 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { NgxScannerQrcodeComponent, NgxScannerQrcodeModule, ScannerQRCodeResult } from 'ngx-scanner-qrcode';
+import { NgxScannerQrcodeComponent, NgxScannerQrcodeModule, ScannerQRCodeDevice, ScannerQRCodeResult } from 'ngx-scanner-qrcode';
 import { TextService } from '../text.service';
-import { filter } from 'rxjs';
+import { filter, take } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NgxScannerQrcodeModule],
+  imports: [CommonModule, NgxScannerQrcodeModule, FormsModule],
   templateUrl: './reader.component.html',
   styleUrls: ['./reader.component.scss']
 })
 export class ReaderComponent implements AfterViewInit {
   title = 'qr-code-reader';
+  selectedDevice?: string;
 
   @ViewChild('action') qrcodeComponent?: NgxScannerQrcodeComponent;
-  @ViewChild('selectDevice') selectDevice?: ElementRef<HTMLSelectElement>;
 
   constructor(
     private readonly router: Router,
@@ -24,11 +25,14 @@ export class ReaderComponent implements AfterViewInit {
   ) {}
 
   ngAfterViewInit() {
-    this.qrcodeComponent?.devices.pipe(filter((devices) => devices.length > 0)).subscribe((devices) => {
+    this.qrcodeComponent?.devices.pipe(
+      filter((devices) => devices.length > 0),
+      take(1)
+    ).subscribe((devices) => {
       const backFacingCamera =
         devices.find((device) => device.label.includes('back'));
-      if (backFacingCamera && this.selectDevice) {
-        this.selectDevice.nativeElement.value = backFacingCamera.deviceId;
+      if (backFacingCamera) {
+        this.selectedDevice = backFacingCamera.deviceId;
       }
     });
     this.qrcodeComponent?.start();
