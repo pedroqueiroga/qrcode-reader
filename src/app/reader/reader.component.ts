3e5f792aa6a684480c1a0fdcd31233ef055e003f
Pedro Queiroga <pedrosqueiroga@gmail.com>
2023-06-06 22:48:59 -0300

    first try

--- src/app/reader/reader.component.ts
+++ src/app/reader/reader.component.ts
@@ -1,21 +1,19 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
-import { NgxScannerQrcodeComponent, NgxScannerQrcodeModule, ScannerQRCodeDevice, ScannerQRCodeResult } from 'ngx-scanner-qrcode';
+import { NgxScannerQrcodeComponent, NgxScannerQrcodeModule, ScannerQRCodeResult } from 'ngx-scanner-qrcode';
 import { TextService } from '../text.service';
 import { filter, take } from 'rxjs';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NgxScannerQrcodeModule, FormsModule],
+  imports: [CommonModule, NgxScannerQrcodeModule],
   templateUrl: './reader.component.html',
   styleUrls: ['./reader.component.scss']
 })
 export class ReaderComponent implements AfterViewInit {
   title = 'qr-code-reader';
-  selectedDevice?: ScannerQRCodeDevice;
 
   @ViewChild('action') qrcodeComponent?: NgxScannerQrcodeComponent;
   @ViewChild('selectDevice') selectDevice?: ElementRef<HTMLSelectElement>;
@@ -33,11 +31,8 @@ export class ReaderComponent implements AfterViewInit {
       const backFacingCamera =
         devices.find((device) => device.label.includes('back'));
       if (backFacingCamera && this.selectDevice) {
-        this.selectedDevice = backFacingCamera;
         this.selectDevice.nativeElement.value = backFacingCamera.deviceId;
         this.selectDevice.nativeElement.dispatchEvent(new Event('change'));
-      } else if (this.selectDevice) {
-        this.selectedDevice = devices[0];
       }
     });
     this.qrcodeComponent?.start();
@@ -50,10 +45,5 @@ export class ReaderComponent implements AfterViewInit {
       this.textService.text = data;
       this.router.navigateByUrl('only-text');
     }
-  }
-
-  onSelectDevice() {
-    this.selectedDevice &&
-      this.qrcodeComponent?.playDevice(this.selectedDevice.deviceId);
   }
 }
